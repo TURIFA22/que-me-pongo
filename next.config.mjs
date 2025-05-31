0452441c8dbd1cb23bdb411b97dd9936cbf3368b
Facundo Turini <turinif@hotmail.com>
2025-05-31 11:12:26 -0300

    App Que me pongo - Version HTML completa

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,10 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  basePath: '/stylemate-app',
+  assetPrefix: '/stylemate-app'
 }
 
 export default nextConfig
